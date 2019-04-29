28460590c1bd6d16a931c4d9e0c2b7d504b45747
CheeHong <tancheehong1990@hotmail.com>
2019-04-29 14:11:59 +0800

    Add name list fragment for selection

---
+++ app/src/main/java/com/chtan/cartrack/activity/NameList.kt
@@ -0,0 +1,47 @@
+package com.chtan.cartrack.activity
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+
+import com.chtan.cartrack.R
+import com.chtan.cartrack.network.RetroDetail
+
+class NameList : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_name_list, container, false)
+        val activity = context as ViewUserDetail
+        setTheListView(view,activity.masterDetailList)
+        return view
+    }
+
+    fun setTheListView(view: View,theList:List<RetroDetail>){
+        var user_name_list=ArrayList<String>()
+        val list_view = view.findViewById<ListView>(R.id.master_name_list)
+        theList.forEach { userDetail: RetroDetail ->
+            user_name_list.add(userDetail.name.toString())
+        }
+
+        val adapter = ArrayAdapter(view.context, R.layout.listview_item, user_name_list)
+        list_view?.adapter = adapter
+        list_view.onItemClickListener = object : AdapterView.OnItemClickListener {
+
+            override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                val itemValue = position
+                val activity = context as ViewUserDetail
+                activity.masterDetailSelection = itemValue
+                activity.showMasterDetail()
+            }
+        }
+    }
+}
