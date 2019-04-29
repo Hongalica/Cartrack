6cfdfa233d249771e1f1ed75f3231eafa7494f2c
CheeHong <tancheehong1990@hotmail.com>
2019-04-29 14:10:05 +0800

    Transform ViewUserDetail to frament activity

--- app/src/main/java/com/chtan/cartrack/activity/ViewUserDetail.kt
+++ app/src/main/java/com/chtan/cartrack/activity/ViewUserDetail.kt
@@ -1,10 +1,13 @@
 package com.chtan.cartrack.activity
 
 import android.app.Activity
-import android.content.Context
 import android.os.Bundle
+import android.os.Parcelable
+import android.support.v4.app.FragmentActivity
 import android.util.Log
+import android.widget.FrameLayout
 import android.widget.TextView
+import android.widget.Toast
 import com.chtan.cartrack.R
 import com.chtan.cartrack.network.JsonPlaceHolderApi
 import com.chtan.cartrack.network.RetroDetail
@@ -15,7 +18,11 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 
-class ViewUserDetail: Activity() {
+class ViewUserDetail: FragmentActivity() {
+
+    var masterDetailList = listOf<RetroDetail>()
+    val manager = supportFragmentManager
+    var masterDetailSelection:Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,34 +30,7 @@ class ViewUserDetail: Activity() {
         queryMasterDetail()
     }
 
-    fun setTheText(theList:List<RetroDetail>){
-        Log.d("Iam herererererere", "muahahahhahaha" + this)
-        var theText = findViewById<TextView>(R.id.text_view_result)
-       theList.forEach { retroDetails: RetroDetail ->
-            var content = ""
-            content += "ID: " + retroDetails.id + "\n"
-            content += "User ID: " + retroDetails.name + "\n"
-            content += "Title: " + retroDetails.username + "\n"
-            content += "Text: " + retroDetails.email + "\n"
-            content += "Street: " + retroDetails.address?.street + "\n"
-            content += "Suite: " + retroDetails.address?.suite + "\n"
-            content += "City: " + retroDetails.address?.city + "\n"
-            content += "Zipcode: " + retroDetails.address?.zipcode + "\n"
-            content += "Latitude: " + retroDetails.address?.geo?.lat + "\n"
-            content += "Longitude: " + retroDetails.address?.geo?.lng + "\n"
-            content += "Phone: " + retroDetails.phone + "\n"
-            content += "Website: " + retroDetails.website + "\n"
-            content += "Company name:" +retroDetails.company?.name + "\n"
-            content += "Company Catchphrase:" +retroDetails.company?.catchPhrase + "\n"
-            content += "Company Business:" +retroDetails.company?.bs + "\n\n"
-
-            theText.append(content)
-        }
-
-    }
-
     fun queryMasterDetail() {
-        var masterDetailList = listOf<RetroDetail>()
         val retrofit = Retrofit.Builder()
             .baseUrl("https://jsonplaceholder.typicode.com")
             .addConverterFactory(GsonConverterFactory.create())
@@ -58,30 +38,37 @@ class ViewUserDetail: Activity() {
 
         val jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi::class.java)
         val call = jsonPlaceHolderApi.getUserDetail()
-        //val retroDetail = call.execute().body()
-
-        // return retroDetail!!
 
         call.enqueue(object: Callback<List<RetroDetail>> {
             override fun onResponse(call: Call<List<RetroDetail>>, response: Response<List<RetroDetail>>){
-
                 if (!response.isSuccessful) {
-                    //  result.setText("Code: " + response.code())
+                    Toast.makeText(applicationContext,("Error Code: " + response.code()), Toast.LENGTH_LONG).show()
                     return
                 }
-
                 val retroDetail = response.body()
                 masterDetailList = retroDetail!!
-                Log.d("just want to check query333", ""+masterDetailList)
-                setTheText(masterDetailList)
-
+                showMasterNameList()
             }
-
             override fun onFailure(call: Call<List<RetroDetail>>, t: Throwable) {
-                // result.text=t.message
+                Toast.makeText(applicationContext,t.message, Toast.LENGTH_LONG).show()
             }
         })
+    }
 
+    fun showMasterNameList() {
+        val transaction = manager.beginTransaction()
+        val fragment = NameList()
+        transaction.replace(R.id.fragment_holder, fragment, "theNamelist")
+        transaction.addToBackStack( null)
+        transaction.commit()
+    }
+
+    fun showMasterDetail() {
+        val transaction = manager.beginTransaction()
+        val fragment = MasterDetail()
+        transaction.replace(R.id.fragment_holder, fragment, "theMasterDetail")
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
 
